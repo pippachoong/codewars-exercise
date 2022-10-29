9ac1227b6760298edcd2a26106a3e9cf2399c378
Philippa Choong <pippachoong@gmail.com>
2022-10-30 10:33:19 +1100

    part solution of rgb-to-hex-convertion

--- rgb-to-hex-convertion.js
+++ rgb-to-hex-convertion.js
@@ -8,3 +8,44 @@
 // rgb(255, 255, 300) # returns FFFFFF
 // rgb(0, 0, 0) # returns 000000
 // rgb(148, 0, 211) # returns 9400D3
+// "0123456789ABCDEF"
+// string to array
+// find index (n)
+
+
+// // step 1 
+const numberToHex = (number) => {
+    let array = "0123456789ABCDEF".split("")
+    // console.log(array);
+    let output = array[number]
+    // console.log(output);
+    return output
+}
+
+// // step 2 get decimal from the value input
+const decimalToHex = (number) => {
+    let remainder = (number) / 16 // e.g.15.3
+    decimalOfRemainder = ((remainder % 1).toFixed(4)) * 16
+    return decimalOfRemainder
+}
+
+// step 3 - get integer fromn the value input
+const decimalToHex = (number) => {
+    let integerValue = (Math.trunc(number)) / 16
+    return integerValue
+}
+
+
+
+// Example 1: (4253)10 = (109D) 16
+
+// 4253 ÷ 16 = 265.8125
+// 0.8125 * 16 = 13(Remainder 13, equivalent to D in hexadecimal)
+// 265 ÷ 16 = 16.5625
+// 0.5625 * 16 = 9(Remainder 9)
+// 16 ÷ 16 = 1(Remainder 0)
+// 1 ÷ 16 = 0.0625
+// 0.00625 * 16: 1(Remainder 1)
+
+// Read the remainders from the most significant to the least - from bottom to top: 109D.
+// 109D the hex equivalent of(4253)10
