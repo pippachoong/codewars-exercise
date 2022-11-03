d22ca64c7c8d76aab55d2f1f9811125e57928d9c
Philippa Choong <pippachoong@gmail.com>
2022-11-03 23:21:01 +1100

    reversed string js

---
+++ reversed-strings.js
@@ -0,0 +1,10 @@
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  => 'dlrow'
+// 'word'   => 'drow'
+
+const solution = (str) => {
+    let stringArray = str.split("").reverse().join
+    return stringArray
+
+}
