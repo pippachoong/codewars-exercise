38b1dbef30c2a7df97d373c889c3fc27f1cd3a9b
Philippa Choong <pippachoong@gmail.com>
2022-11-07 23:23:13 +1100

    split-string rb

--- split-string.rb
+++ split-string.rb
@@ -1,3 +1,19 @@
 def solution(str)
 
+    str_array = str.split("")
+    output = []
+
+    if str_array.length % 2 === 1
+        str_array.push("_")
+    end
+
+    while str_array.length >= 2
+        pair_string = str_array.slice!(0,2).join("") # grad first 2 elements
+        output.push(pair_string)
+    end
+
+    return output
+
+
+
 end
